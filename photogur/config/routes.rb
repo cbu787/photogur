492a600ff83957c45ddf441f66e6ea3e2900b0c1
CALLUM BURNS <CBU787@GMAIL.COM>
2016-04-06 17:52:46 -0400

    Completed item 12; added destroy action in controller; created link to picture in show.html.erb. Added feature to delete pictures.

--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -11,6 +11,10 @@ Rails.application.routes.draw do
   get "pictures/:id/edit" => "pictures#edit", as: "picture"
   patch "pictures/:id" => "pictures#update"
 
+  delete "pictures/:id" => "pictures#destroy", as: "delete_picture"
+
+  root "pictures#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
